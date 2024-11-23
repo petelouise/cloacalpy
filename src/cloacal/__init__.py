07c40f863d90e7a5c8c46f75921aa16d30ea2ee0
petelouise <programmetress@petelouise.beauty>
2024-11-23 08:20:05 -0600

    add load and import from parent module

--- src/cloacal/__init__.py
+++ src/cloacal/__init__.py
@@ -1,2 +1,13 @@
-def hello() -> str:
-    return "Hello from cloacal!"
+from collections import OrderedDict
+from typing import Any
+
+from .format import format_dict, format_str
+from .parse import parse
+
+
+def load(path: str) -> OrderedDict[Any, Any]:
+    with open(path, "r") as f:
+        return parse(f.read())
+
+
+__all__ = ["parse", "format_str", "format_dict"]
